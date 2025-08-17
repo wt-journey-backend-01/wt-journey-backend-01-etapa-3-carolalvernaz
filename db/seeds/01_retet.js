c489cec655378eb08dbcf4fbef854da85fb8d39d
Caroline Alvernaz <carolinefreitasalvernaz@gmail.com>
2025-08-17 20:38:19 -0300

    Estrutura finalizada com controllers e seeds organizados

--- db/seeds/01_retet.js
+++
@@ -1,5 +0,0 @@
-exports.seed = async function (knex) {
-  // Limpa tabelas e reseta IDs (ordem certa para não quebrar FK)
-  await knex.raw('TRUNCATE TABLE casos RESTART IDENTITY CASCADE');
-  await knex.raw('TRUNCATE TABLE agentes RESTART IDENTITY CASCADE');
-};
